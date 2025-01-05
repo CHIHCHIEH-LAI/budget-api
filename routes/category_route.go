3cae7f029050d8c6ca6d6fd66fb8c21cccbf45e3
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2025-01-05 21:45:06 +0800

    Add category routes for managing categories

---
+++ routes/category_route.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"budget_manager/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+// RegisterCategoryRoutes registers category-related routes
+func RegisterCategoryRoutes(router *gin.Engine) {
+	categoryRoutes := router.Group("/categories")
+	{
+		categoryRoutes.GET("", controllers.ListCategories)
+		categoryRoutes.POST("", controllers.CreateCategory)
+		categoryRoutes.PUT("/:id", controllers.UpdateCategory)
+		categoryRoutes.DELETE("/:id", controllers.DeleteCategory)
+	}
+}
