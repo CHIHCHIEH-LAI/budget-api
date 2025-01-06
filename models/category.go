7c7618d69e3e5962797d506391599a8ac133c067
CHIHCHIEH-LAI <62993209+CHIHCHIEH-LAI@users.noreply.github.com>
2025-01-06 15:58:31 +0800

    Enhance validation for budget and expense fields in category models to ensure non-negative values

--- models/category.go
+++ models/category.go
@@ -2,21 +2,21 @@ package models
 
 type CategoryCreate struct {
 	Name    string  `json:"name" binding:"required"`
-	Budget  float64 `json:"budget" binding:"required"`
-	Expense float64 `json:"expense" default:"0"`
+	Budget  float64 `json:"budget" binding:"required,gte=0"`
+	Expense float64 `json:"expense" default:"0" binding:"gte=0"`
 }
 
 type CategoryBudgetUpdate struct {
-	Budget float64 `json:"budget" binding:"required"`
+	Budget float64 `json:"budget" binding:"required,gte=0"`
 }
 
 type CategoryExpenseUpdate struct {
-	Expense float64 `json:"expense" binding:"required"`
+	Expense float64 `json:"expense" binding:"required,gte=0"`
 }
 
 type Category struct {
 	ID      uint    `json:"id" binding:"required"`
 	Name    string  `json:"name" binding:"required"`
-	Budget  float64 `json:"budget" binding:"required"`
-	Expense float64 `json:"expense" binding:"required"`
+	Budget  float64 `json:"budget" binding:"required,gte=0"`
+	Expense float64 `json:"expense" binding:"required,gte=0"`
 }
