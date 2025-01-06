661394e425be7e91775709181b2ef04bd99ae1fa
CHIHCHIEH-LAI <62993209+CHIHCHIEH-LAI@users.noreply.github.com>
2025-01-06 14:31:39 +0800

    Enhance category model with budget and expense fields for improved data management

--- models/category.go
+++ models/category.go
@@ -1,20 +1,22 @@
 package models
 
 type CategoryCreate struct {
-	Name   string  `json:"name" binding:"required"`
-	Budget float64 `json:"budget" binding:"required"`
-	Used   float64 `json:"used" default:"0"`
+	Name    string  `json:"name" binding:"required"`
+	Budget  float64 `json:"budget" binding:"required"`
+	Expense float64 `json:"expense" default:"0"`
 }
 
-type CategoryUpdate struct {
-	Name   string  `json:"name" default:""`
-	Budget float64 `json:"budget" default:"0"`
-	Used   float64 `json:"used" default:"0"`
+type CategoryBudgetUpdate struct {
+	Budget float64 `json:"budget" binding:"required"`
+}
+
+type CategoryExpenseUpdate struct {
+	Expense float64 `json:"expense" binding:"required"`
 }
 
 type Category struct {
-	ID     uint    `json:"id" binding:"required"`
-	Name   string  `json:"name" binding:"required"`
-	Budget float64 `json:"budget" binding:"required"`
-	Used   float64 `json:"used" binding:"required"`
+	ID      uint    `json:"id" binding:"required"`
+	Name    string  `json:"name" binding:"required"`
+	Budget  float64 `json:"budget" binding:"required"`
+	Expense float64 `json:"expense" binding:"required"`
 }
